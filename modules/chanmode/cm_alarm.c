fdd7d23e34d9ff1b4a02b35547d8d70991f8530b
Roman Senn <roman.l.senn@gmail.com>
2014-05-06 15:05:39 +0000

    added +g usermode, and login log message

--- modules/chanmode/cm_alarm.c
+++ modules/chanmode/cm_alarm.c
@@ -29,7 +29,13 @@ static int cm_alarm_msg_hook(struct client   *cptr, struct channel *acptr,
                              intptr_t         type, const char     *text);
 
 static int cm_alarm_join_hook(struct lclient *lcptr, struct client *cptr,
-                               struct channel *chptr);
+                              struct channel *chptr);
+static int cm_alarm_bounce   (struct lclient        *lcptr,                                                                                                                
+                              struct client         *cptr,                                                                                                                 
+                              struct channel        *chptr,                                                                                                                
+                              struct chanuser       *cuptr,                                                                                                                
+                              struct list           *lptr,                                                                                                                 
+                              struct chanmodechange *cmcptr);                                                                                                              
 
 /* -------------------------------------------------------------------------- *
  * -------------------------------------------------------------------------- */
@@ -46,9 +52,11 @@ static struct chanmode cm_alarm_mode =
   CHANMODE_TYPE_SINGLE,    /* Channel mode is a single flag */
   CHFLG(o),                /* Only OPs can change the flag */
   0,                       /* No order and no reply */
-  chanmode_bounce_simple,  /* Bounce handler */
+  cm_alarm_bounce,         /* Bounce handler */
   cm_alarm_help            /* Help text */
 };
+
+static int cm_alarm_event_log;
 
 /* -------------------------------------------------------------------------- *
  * Module hooks                                                               *
@@ -58,6 +66,10 @@ int cm_alarm_load(void)
   /* register the channel mode */
   if(chanmode_register(&cm_alarm_mode) == NULL)
     return -1;
+
+  cm_alarm_event_log = log_source_find("event");                                                                                                              
+  if(cm_alarm_event_log == -1)                                                                                                                                
+    cm_alarm_event_log = log_source_register("event");            
 
   hook_register(channel_message, HOOK_DEFAULT, cm_alarm_msg_hook);
   hook_register(channel_join, HOOK_4TH, cm_alarm_join_hook);
@@ -69,6 +81,9 @@ void cm_alarm_unload(void)
 {
   /* unregister the channel mode */
   chanmode_unregister(&cm_alarm_mode);
+
+  log_source_unregister(cm_alarm_event_log);
+  cm_alarm_event_log = -1;
 
   hook_unregister(channel_join, HOOK_4TH, cm_alarm_join_hook);
   hook_unregister(channel_message, HOOK_DEFAULT, cm_alarm_msg_hook);
@@ -88,4 +103,23 @@ static int cm_alarm_join_hook(struct lclient *lcptr, struct client *cptr,
                               struct channel *chptr)
 {
 	return 0;
+}
+
+/* -------------------------------------------------------------------------- *
+ * -------------------------------------------------------------------------- */
+static int cm_alarm_bounce   (struct lclient        *lcptr,                                                                                                                
+                              struct client         *cptr,                                                                                                                 
+                              struct channel        *chptr,                                                                                                                
+                              struct chanuser       *cuptr,                                                                                                                
+                              struct list           *lptr,                                                                                                                
+                              struct chanmodechange *cmcptr)
+{
+  if(cmcptr->what == CHANMODE_ADD)                                                                                                                                        
+  {
+    if(!(chptr->modes & CHFLG(A))) {
+      log(cm_alarm_event_log, L_warning, "Alarm triggered in %s", chptr->name);
+    }
+  }
+
+  return chanmode_bounce_simple(lcptr, cptr, chptr, cuptr, lptr, cmcptr);
 }
