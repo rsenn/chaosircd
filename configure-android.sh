d68a44c1efad314a47f7ec841cfe4eee49b4cf30
Roman Senn <roman.l.senn@gmail.com>
2014-07-05 08:18:36 +0200

    ...

--- configure-android.sh
+++ configure-android.sh
@@ -11,3 +11,20 @@ host=arm-linux-androideabi
 	--build=`gcc -dumpmachine` \
 	"$@" \
 	CC="${host}-gcc -I$ANDROID_NDK_ROOT/platforms/android-16/arch-arm/usr/include -L$ANDROID_NDK_ROOT/platforms/android-16/arch-arm/usr/lib"
+
+cat <<EOF | tee build-android.sh |sed -u "s|^|build-android.sh: |"
+#!/bin/sh
+
+DESTDIR="\$PWD-android"
+
+rm -rf "\$DESTDIR"
+
+make clean
+./config.status
+make
+make DESTDIR="\$DESTDIR" install
+
+cmd='tar -C "\$DESTDIR" -c . |xz -v -e -6 -f -c >"\${DESTDIR##*/}.txz"'
+eval "echo \"\$cmd\"; \$cmd"
+
+EOF
