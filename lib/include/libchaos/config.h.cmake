6d2a845077e01544a62bcca3012c05216166a145
Roman Senn <roman.l.senn@gmail.com>
2014-09-20 08:08:30 +0000

    ...

--- lib/include/libchaos/config.h.cmake
+++ lib/include/libchaos/config.h.cmake
@@ -1,3 +1,29 @@
+#if 1
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
+
+/* Define to the release name of this package. */
+#cmakedefine PACKAGE_RELEASE "@PACKAGE_RELEASE@"
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME "@PACKAGE_TARNAME@"
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL "@PACKAGE_URL@"
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
+
+/* Platform this server runs on */
+#cmakedefine PLATFORM "@PLATFORM@"
+#endif
+
 /* Use select() for i/o multiplexing */
 #cmakedefine USE_SELECT 1
 
@@ -63,3 +89,4 @@
 
 /* Dynamic linkable library filename extension */
 #cmakedefine DLLEXT "@DLLEXT@"
+
