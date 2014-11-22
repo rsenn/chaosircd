8a8429ad7ad22eebc337345ea3c8d2c5e34e08d5
Roman Senn <rls@crowdguard.org>
2014-11-22 09:59:02 +0100

    ...

--- libchaos/cmake/ConfigDlFcn.cmake
+++
@@ -1,7 +0,0 @@
-include(CheckLibraryExists)
-
-check_library_exists(dl dlsym "" HAVE_DLFCN)
-
-if(HAVE_DLFCN)
-  set(DLFCN_LIBRARIES dl)
-endif()
--- libchaos/cmake/ConfigMathLib.cmake
+++
@@ -1,7 +0,0 @@
-include(CheckLibraryExists)
-
-check_library_exists(m sin "" HAVE_MATH)
-
-if(HAVE_MATH)
-  set(MATH_LIBRARIES m)
-endif()
