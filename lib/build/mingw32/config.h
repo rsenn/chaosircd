b0140d34d8d195284d078a6b69fdebe9b89752cc
Roman Senn <rls@crowdguard.org>
2014-12-01 19:38:42 +0000

    ...

--- lib/build/mingw32/config.h
+++ lib/build/mingw32/config.h
@@ -1,38 +1,5 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.in by autoheader.  */
-
-/* Define if building universal (internal helper macro) */
-/* #undef AC_APPLE_UNIVERSAL_BUILD */
-
-/* Define this if you have BSD-type socket filters */
-/* #undef BSD_SOCKET_FILTER */
-
-/* Creation time of this server */
-#define CREATION "Mon Dec  1 15:15:27     2014"
-
-/* Define your machines endian */
-#define ENDIAN ENDIAN_LIL
-
-/* Big endian machine */
-#define ENDIAN_BIG 1
-
-/* Little endian machine */
-#define ENDIAN_LIL 0
-
-/* Define to 1 if you have the <cygwin/in.h> header file. */
-/* #undef HAVE_CYGWIN_IN_H */
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-/* #undef HAVE_DLFCN_H */
-
-/* Define to 1 if you have the <elf.h> header file. */
-/* #undef HAVE_ELF_H */
-
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
-
-/* Define this if you have freetype 2 */
-/* #undef HAVE_FT2 */
 
 /* Define to 1 if you have the `getpagesize' function. */
 #define HAVE_GETPAGESIZE 1
@@ -46,12 +13,6 @@
 /* Define to 1 if you have the `crypto' library (-lcrypto). */
 #define HAVE_LIBCRYPTO 1
 
-/* Define to 1 if you have the `dl' library (-ldl). */
-/* #undef HAVE_LIBDL */
-
-/* Define to 1 if you have the `duma' library (-lduma). */
-/* #undef HAVE_LIBDUMA */
-
 /* Define to 1 if you have the `m' library (-lm). */
 #define HAVE_LIBM 1
 
@@ -64,47 +25,17 @@
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
 
-/* Define to 1 if you have the <linux/filter.h> header file. */
-/* #undef HAVE_LINUX_FILTER_H */
-
-/* Define to 1 if you have the <linux/types.h> header file. */
-/* #undef HAVE_LINUX_TYPES_H */
-
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have a working `mmap' system call. */
-/* #undef HAVE_MMAP */
-
-/* Define this if you have MySQL */
-/* #undef HAVE_MYSQL */
-
-/* Define to 1 if you have the <netinet/in.h> header file. */
-/* #undef HAVE_NETINET_IN_H */
-
-/* Define to 1 if you have the <net/bpf.h> header file. */
-/* #undef HAVE_NET_BPF_H */
-
-/* Define to 1 if you have the <net/ethernet.h> header file. */
-/* #undef HAVE_NET_ETHERNET_H */
-
-/* Define this if you have PostgreSQL */
-/* #undef HAVE_PGSQL */
 
 /* Define to 1 if you have the <signal.h> header file. */
 #define HAVE_SIGNAL_H 1
 
-/* Define this if you have the socketpair() function */
-/* #undef HAVE_SOCKETPAIR */
-
-/* Define this if your OS has a socket-based filtering mechanism */
-/* #undef HAVE_SOCKET_FILTER */
-
 /* Define this if you have SQLite */
-//#define HAVE_SQLITE "1"
+/* #define HAVE_SQLITE 1 */
 
 /* Define this if you have OpenSSL */
-/* #undef HAVE_SSL */
+#define HAVE_SSL 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -118,29 +49,8 @@
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
-/* Define this if you have the strlcat() function */
-/* #undef HAVE_STRLCAT */
-
-/* Define this if you have the strlcpy() function */
-/* #undef HAVE_STRLCPY */
-
-/* Define to 1 if you have the <sys/ioctl.h> header file. */
-/* #undef HAVE_SYS_IOCTL_H */
-
-/* Define to 1 if you have the <sys/mman.h> header file. */
-/* #undef HAVE_SYS_MMAN_H */
-
 /* Define to 1 if you have the <sys/param.h> header file. */
 #define HAVE_SYS_PARAM_H 1
-
-/* Define to 1 if you have the <sys/poll.h> header file. */
-/* #undef HAVE_SYS_POLL_H */
-
-/* Define to 1 if you have the <sys/select.h> header file. */
-/* #undef HAVE_SYS_SELECT_H */
-
-/* Define to 1 if you have the <sys/socket.h> header file. */
-/* #undef HAVE_SYS_SOCKET_H */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -154,17 +64,8 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
-/* Define to 1 if you have the <sys/wait.h> header file. */
-/* #undef HAVE_SYS_WAIT_H */
-
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
-
-/* Define this if you have Linux-type socket filters */
-/* #undef LINUX_SOCKET_FILTER */
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "libchaos"
@@ -185,7 +86,7 @@
 #define PACKAGE_VERSION "2.1.0"
 
 /* Platform this server runs on */
-#define PLATFORM "MINGW32_NT-5.1 MiniXP-481 1.0.18(0.48/3/2) 2012-11-21 22:34 i686 Msys"
+#define PLATFORM "MINGW32_NT-5.1 MiniXP-115 1.0.18(0.48/3/2) 2012-11-21 22:34 i686 Msys"
 
 /* The size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 4
@@ -199,20 +100,5 @@
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
-/* Use poll() for i/o multiplexing */
-/* #undef USE_POLL */
-
 /* Use select() for i/o multiplexing */
 #define USE_SELECT 1
-
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-#if defined AC_APPLE_UNIVERSAL_BUILD
-# if defined __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# endif
-#else
-# ifndef WORDS_BIGENDIAN
-/* #  undef WORDS_BIGENDIAN */
-# endif
-#endif
